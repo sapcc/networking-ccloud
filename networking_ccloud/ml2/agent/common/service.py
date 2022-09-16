12b3be9a48e051bc47c488de4548eed970edd914
Johannes Kulik <johannes.kulik@sap.com>
2022-09-16 16:18:00 +0200

    Support starting the agent with threading
    This module bundles some functionality of oslo_service, neutron.service
    and neutron_lib.rpc into a single class to be able to start the agent
    with threading instead of eventlet.
    Especially the signal-handling build deeply into the stack creates the
    need to call all functionality on our own. There's still some room for
    improvement in what the main thread does, as it's currently only
    sleeping during the main work to wait for signals to arrive.

---
+++ networking_ccloud/ml2/agent/common/service.py
@@ -0,0 +1,172 @@
+# Copyright 2022 SAP SE
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import inspect
+import os
+import random
+import signal
+import time
+
+from neutron.common import profiler as neutron_profiler
+from neutron_lib import context
+from oslo_config import cfg
+from oslo_log import log as logging
+from oslo_service import loopingcall
+from osprofiler import profiler
+
+from networking_ccloud.ml2.agent.common.loopingcall import monkeypatch_loopingcall
+from networking_ccloud.ml2.agent.common.rpc import setup_rpc, shutdown_rpc
+
+LOG = logging.getLogger(__name__)
+
+
+@profiler.trace_cls("rpc")
+class ThreadedService:
+    """Bundle together the main parts to start a manager
+
+    This class contains the extracted parts of neutron.service.Service,
+    neutron_lib.rpc.Service and oslo_service.service that are necessary to
+    bring up a manager for our use-case. We needed to extract them, because the
+    mentioned classes are too coupled with eventlet/greenthreads.
+    """
+    def __init__(self, binary, topic, manager, host=None, report_interval=None,
+                 periodic_interval=None, periodic_fuzzy_delay=None):
+        if not host:
+            host = cfg.CONF.host
+        if not binary:
+            binary = os.path.basename(inspect.stack()[-1][1])
+        if not topic:
+            topic = binary.rpartition('neutron-')[2]
+            topic = topic.replace("-", "_")
+        if report_interval is None:
+            report_interval = cfg.CONF.report_interval
+        if periodic_interval is None:
+            periodic_interval = cfg.CONF.periodic_interval
+        if periodic_fuzzy_delay is None:
+            periodic_fuzzy_delay = cfg.CONF.periodic_fuzzy_delay
+
+        self.binary = binary
+        self.topic = topic
+        self.report_interval = report_interval
+        self.periodic_interval = periodic_interval
+        self.periodic_fuzzy_delay = periodic_fuzzy_delay
+        self.manager = manager
+        # set to the Connection instance containing the RPC servers
+        self.conn = None
+        # contains the started loopingcalls
+        self.timers = []
+
+        monkeypatch_loopingcall()
+
+        neutron_profiler.setup(binary, host)
+
+        signal.signal(signal.SIGHUP, self._signal_ignore)
+        signal.signal(signal.SIGTERM, self._signal_graceful_exit)
+        signal.signal(signal.SIGINT, self._signal_fast_exit)
+
+        # conf.register_opts(_options.service_opts)
+        # TODO(jkulik) do we need to log all config options?
+        # TODO(jkulik) do we need a graceful shutdown timeout?
+
+        # TODO(jkulik) add a backdoor thread
+        # self.backdoor_port = (
+        #     eventlet_backdoor.initialize_if_enabled(self.conf))
+
+    def _signal_ignore(self, signo, frame):
+        LOG.info('Caught SIGHUP signal, ignoring it')
+
+    def _signal_graceful_exit(self, signo, frame):
+        LOG.info('Caught SIGTERM signal, stopping service')
+        self.stop()
+        self.wait()
+
+    def _signal_fast_exit(self, signo, frame):
+        LOG.info('Caught SIGINT signal, instantaneous exiting')
+        os._exit(1)
+
+    def start(self):
+        """Start the service
+
+        - let the manager run custom pre-start
+        - start rpc listener/server
+        - start _report_state background task
+        - start periodic tasks runner
+        - let manager run custom post-start
+        """
+        self.manager.init_host()
+        self.conn = setup_rpc(self.topic, self.manager)
+
+        if self.report_interval:
+            pulse = loopingcall.FixedIntervalLoopingCall(self.manager._report_state)
+            pulse.start(interval=self.report_interval,
+                        initial_delay=self.report_interval)
+            self.timers.append(pulse)
+
+        if self.periodic_interval:
+            if self.periodic_fuzzy_delay:
+                initial_delay = random.randint(0, self.periodic_fuzzy_delay)
+            else:
+                initial_delay = None
+
+            periodic = loopingcall.FixedIntervalLoopingCall(
+                self.periodic_tasks)
+            periodic.start(interval=self.periodic_interval,
+                           initial_delay=initial_delay)
+            self.timers.append(periodic)
+
+        self.manager.after_start()
+
+    def wait(self):
+        """Wait for the service to get stopped"""
+        # rpc-server is waited for in the Connection.close() already
+
+        # wait for our loopingcalls to stop
+        for x in self.timers:
+            try:
+                x.wait()
+            except Exception:
+                LOG.exception("Exception occurs when waiting for timer")
+
+    def run(self):
+        """Start the server and wait for any signals to arrive"""
+        self.start()
+        try:
+            while True:
+                # TODO(jkulik) we could check if the threads we expect to be alive are
+                # still alive here, e.g. if our connection still has listening
+                # servers and if our self.timers still have running threads
+                time.sleep(1)
+        finally:
+            self.stop()
+            self.wait()
+
+    def stop(self):
+        """Stop this service
+
+        - stop the rpc-server
+        - stop all timers/threads/periodics
+        - run custom stop actions the manager needs to do
+        """
+        shutdown_rpc(self.conn)
+        for x in self.timers:
+            try:
+                x.stop()
+            except Exception:
+                LOG.exception("Exception occurs when timer stops")
+        self.timers = []
+        self.manager.stop()
+
+    def periodic_tasks(self, raise_on_error=False):
+        """Tasks to be run at a periodic interval."""
+        ctxt = context.get_admin_context()
+        self.manager.periodic_tasks(ctxt, raise_on_error=raise_on_error)
