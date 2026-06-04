0f117375ab2189d1fea066041d76d8af222513ba
Sebastian Lohff <sebastian.lohff@sap.com>
2026-06-04 15:34:29 +0200

    Adapt config checker for Kubernetes
    The driver itself contains a lot of validation embedded in pydantic.
    This validation is called on config load. Our main focus with the config
    checker is to check if the driver would actually start with a given
    config, thus we're limiting the scope of the checker to the driver
    config and the optional credentials config.
    
    The config generator already generates configs in a way that we can
    easily put them into a Kubernetes ConfigMap, so the checker can now
    unwrap this via command line parameters. The config is loaded, parsed,
    unwrapped and then written to a temporary file, which will be deleted
    after attempting to re-load it.

--- networking_ccloud/tools/config_check.py
+++ networking_ccloud/tools/config_check.py
@@ -13,51 +13,82 @@
 #    under the License.
 
 import argparse
+import contextlib
+import os
 import sys
+import tempfile
 
 from oslo_config import cfg
+import yaml
 
 from networking_ccloud.common.config import get_driver_config
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="This tool can be used to check if a config file for the driver would actually be "
-                    "loadable by the driver. You also have the option to only check the yaml part of "
-                    "the config separately."
+        description="Check if a driver config is loadable by the driver. The tool does the same validation "
+                    "that is also done by the driver on startup."
     )
-    parser.add_argument("-c", "--config-file")
-    parser.add_argument("-y", "--yaml-file")
-    parser.add_argument("--credentials-file")
+    parser.add_argument("-d", "--driver-config", required=True, help="Path to driver config yaml")
+    parser.add_argument("--ud", "--unwrap-driver-config",
+                        help="Unwrap config dict keys (separated with /), useful when reading config from a ConfigMap")
+    parser.add_argument("--credentials-config", help="Path to driver credentials yaml")
+    parser.add_argument("--uc", "--unwrap-credentials", help="Unwrap credentials dict keys (separated with /)")
 
     args = parser.parse_args()
 
-    if not (args.config_file or args.yaml_file):
-        parser.error("Please specify either a config file or a yaml file to check")
-    elif args.config_file and (args.yaml_file or args.credentials_file):
-        parser.error("Config file and yaml file / credentials file checks via cli are mutually exclusive")
+    def _traverse_dict(data, path, item_name):
+        for key in path.split("/"):
+            if not isinstance(data, dict):
+                print(f"ERROR: found non-dict object in {item_name} path, cannot unpack")
+                sys.exit(1)
+            if key not in data:
+                print(f"ERROR: missing key '{key}' in {item_name}, cannot unpack")
+                sys.exit(1)
+            data = data[key]
+        return data
 
-    if args.config_file:
-        # register necessary opts by importing our olso config
-        from networking_ccloud.common.config import config_oslo  # noqa: F401
+    with tempfile.NamedTemporaryFile(mode="w", delete=False) as conf_file, \
+            tempfile.NamedTemporaryFile(mode="w", delete=False) as creds_file:
 
-        # use the normal driver config file loading facilities
+        # TODO(seba): on py3.12 replace this try-finally with delete_on_close
         try:
-            cfg.CONF(args=["--config-file", args.config_file])
-        except cfg.Error as e:
-            print(f"ERROR - Could not load oslo.config: {e}")
-            sys.exit(1)
+            # driver config
+            if args.ud:
+                with open(args.driver_config) as f:
+                    y = yaml.safe_load(f)
+                y = _traverse_dict(y, args.ud, "driver config")
+                yaml.dump(y, conf_file)
+                conf_file.close()
+                args.driver_config = conf_file.name
+            cfg.CONF.set_override('driver_config_path', args.driver_config, group='ml2_cc_fabric')
 
-        print("OK - oslo.config could load driver config")
+            # credentials config
+            if args.credentials_config:
+                if args.uc:
+                    with open(args.credentials_config) as f:
+                        y = yaml.safe_load(f)
+                    y = _traverse_dict(y, args.uc, "driver credentials")
+                    yaml.dump(y, creds_file)
+                    creds_file.close()
+                    args.credentials_config = creds_file.name
+                cfg.CONF.set_override('driver_config_credentials_path', args.credentials_config, group='ml2_cc_fabric')
 
-    if args.credentials_file:
-        cfg.CONF.set_override('driver_config_credentials_path', args.credentials_file, group='ml2_cc_fabric')
+            # load driver config
+            try:
+                drv_conf = get_driver_config(cached=False)
+            except Exception as e:
+                print(f"ERROR - could not load driver config: {type(e)} {e}")
+                sys.exit(1)
 
-    # load yaml config (either via oslo config (args.yaml_file is None) or via path)
-    drv_conf = get_driver_config(path=args.yaml_file, cached=False)
-    print("OK - could load yaml config file")
-    print(f"INFO - Config has {len(drv_conf.switchgroups)} switchgroups and "
-          f"{len(drv_conf.hostgroups)} hostgroups")
+            print("OK - driver config is valid")
+            print(f"INFO - driver config has {len(drv_conf.switchgroups)} switchgroups and "
+                  f"{len(drv_conf.hostgroups)} hostgroups")
+        finally:
+            with contextlib.suppress(Exception):
+                os.remove(conf_file.name)
+            with contextlib.suppress(Exception):
+                os.remove(creds_file.name)
 
 
 if __name__ == '__main__':
